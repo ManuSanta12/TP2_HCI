5436699947cb8204f35407262dd72b3e48069a94
kmenshikoff <kmenshikoff@itba.edu.ar>
2024-04-25 22:39:24 -0300

    started nav bar and devices and automations

--- hci-project/src/App.vue
+++ hci-project/src/App.vue
@@ -1,13 +1,45 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-navigation-drawer app fixed :width=194>
+        <v-list>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">CoolHome</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <divider></divider>
+          <v-list-item-group>
+            <v-list-item v-for="item in items" :key="item.title" link :prepend-icon="item.icon">
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>      
+      </v-navigation-drawer>  
+      <v-container>
+        <v-list-item >
+            <v-list-item-title class="text-h6">Home</v-list-item-title>
+            <v-list-item-subtitle>Add devices and automations to this screen by checking “show in home” in a device or automation</v-list-item-subtitle>
+          </v-list-item>
+          <v-divider></v-divider>
+        </v-container>
+        <DeviceCard />
+        <AutomationsCard />
     </v-main>
-
-    <AppFooter />
   </v-app>
 </template>
 
 <script setup>
-  //
+import AutomationsCard from './components/AutomationsCard.vue';
+import DeviceCard from './components/DeviceCard.vue';
+import { ref } from 'vue';
+const items = ref([
+  { title: "Home", icon: "mdi-home"},
+  { title: "Devices", icon: "mdi-run" },
+  { title: "Automations", icon: "mdi-book"},
+  { title: "Settings", icon: "mdi-heart" }
+]);
 </script>
+
---
+++ hci-project/src/components/AutomationsCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row dense>
+    <v-col cols="12" md="4">
+      <v-card class="pa-2">
+        <v-row class="px-3 pt-3">
+          <v-col cols="10">
+            <v-card-title class="pa-0">Close Curtains</v-card-title>
+          </v-col>
+          <v-col cols="2" class="d-flex justify-end pa-0">
+            <v-btn icon :color="iconColor" @click="togglePlay">
+              <v-icon>{{ icon }}</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-card-subtitle class="px-3">Not Scheduled</v-card-subtitle>
+        <v-card-text class="px-3 pb-3">Includes x actions</v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      icon: 'mdi-pause',
+      iconColor: 'red',
+    };
+  },
+  methods: {
+    togglePlay() {
+      this.icon = this.icon === 'mdi-pause' ? 'mdi-play' : 'mdi-pause';
+      this.iconColor = this.icon == 'mdi-pause' ? 'green' : 'red';
+    },
+  },
+};
+</script>
