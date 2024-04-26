bd2b07981604ee86bb303dea4d183e0feb5ab711
kmenshikoff <kmenshikoff@itba.edu.ar>
2024-04-25 22:44:10 -0300

    made nav bar component

--- hci-project/src/App.vue
+++ hci-project/src/App.vue
@@ -1,23 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <v-navigation-drawer app fixed :width=194>
-        <v-list>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6">CoolHome</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <divider></divider>
-          <v-list-item-group>
-            <v-list-item v-for="item in items" :key="item.title" link :prepend-icon="item.icon">
-                <v-list-item-content>
-                  <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>      
-      </v-navigation-drawer>  
+      <NavigationBar/>
       <v-container>
         <v-list-item >
             <v-list-item-title class="text-h6">Home</v-list-item-title>
@@ -34,12 +18,5 @@
 <script setup>
 import AutomationsCard from './components/AutomationsCard.vue';
 import DeviceCard from './components/DeviceCard.vue';
-import { ref } from 'vue';
-const items = ref([
-  { title: "Home", icon: "mdi-home"},
-  { title: "Devices", icon: "mdi-run" },
-  { title: "Automations", icon: "mdi-book"},
-  { title: "Settings", icon: "mdi-heart" }
-]);
+import NavigationBar from './components/NavigationBar.vue';
 </script>
-
---
+++ hci-project/src/components/NavigationBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-navigation-drawer app fixed :width=194>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">CoolHome</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item-group>
+          <v-list-item v-for="item in items" :key="item.title" link :prepend-icon="item.icon">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>      
+    </v-navigation-drawer>  
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    name: 'Sidebar',
+    setup() {
+      const items = ref([
+        { title: "Home", icon: "mdi-home"},
+        { title: "Devices", icon: "mdi-run" },
+        { title: "Automations", icon: "mdi-book"},
+        { title: "Settings", icon: "mdi-heart" }
+      ]);
+  
+      return { items };
+    },
+  };
+  </script>
+  
