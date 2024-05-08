58a30d0293c8f3be5d21a5b2558f5a663f8a977f
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-05-08 15:10:57 -0300

    restaure device view

---
+++ hci-project/src/Views/DeviceView.vue
@@ -0,0 +1,27 @@
+<script>
+import SpeakerDeviceCard from '@/components/SpeakerDeviceCard.vue';
+import AirConditionerDeviceCard from '@/components/SpeakerDeviceCard.vue';
+import SprinklerDeviceCard from '@/components/SprinklerDeviceCard.vue';
+</script>
+<template>
+<v-app-bar title="Device"  color='#DDEAF4'/>
+  <v-layout class="rounded rounded-md">
+    <v-main color='#DDEAF4'>
+      <v-row class="pa-6 scrollable" >
+        <SpeakerDeviceCard class="ma-2"/>
+        <LightDeviceCard class="ma-2"/>
+        <AirConditionerDeviceCard class="ma-2"/>
+        <SprinklerDeviceCard class="ma-2"/>
+      </v-row>
+    </v-main>
+  </v-layout>
+</template>
+<style>
+.background{
+    background-color: var(--v-background-base) !important;
+}
+.navigation-drawer-background{
+    background-color:'#D8D7D7' !important;
+}
+</style>
+
