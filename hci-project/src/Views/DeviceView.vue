4ac69c787e3123a8789a4a4bfd310968285d40cd
kmenshikoff <kmenshikoff@itba.edu.ar>
2024-05-09 22:12:24 -0300

    started add new device

--- hci-project/src/Views/DeviceView.vue
+++ hci-project/src/Views/DeviceView.vue
@@ -1,10 +1,5 @@
-<script>
-import SpeakerDeviceCard from '@/components/SpeakerDeviceCard.vue';
-import AirConditionerDeviceCard from '@/components/SpeakerDeviceCard.vue';
-import SprinklerDeviceCard from '@/components/SprinklerDeviceCard.vue';
-</script>
+
 <template>
-<v-app-bar title="Device"  color='#DDEAF4'/>
   <v-layout class="rounded rounded-md">
     <v-main color='#DDEAF4'>
       <v-row class="pa-6 scrollable" >
@@ -15,6 +10,42 @@ import SprinklerDeviceCard from '@/components/SprinklerDeviceCard.vue';
       </v-row>
     </v-main>
   </v-layout>
+  <v-dialog v-model="dialog" max-width="1300" scrollable>
+    <template v-slot:activator="{ props: addNew }">
+      <v-app-bar title="Devices"  color='#DDEAF4'>
+        <v-btn rounded prepend-icon="mdi-plus" variant="tonal"  v-bind ="addNew">Add new</v-btn>
+      </v-app-bar>
+    </template>
+    <v-row justify="center">
+      <v-col cols="12" sm="10" md="8" lg="6">
+      <v-card>
+        <v-card-title>Add a new device</v-card-title>
+        <v-divider/>
+        <v-text-field placeholder="Title"  class="pa-3" ></v-text-field>
+        <v-row class="px-3 py-1 align-center"> <!-- Adjust vertical padding and alignment -->
+              <v-col cols="10" class="d-flex align-center"> <!-- Use flexbox to align items vertically -->
+                <v-list-item class="pa-0 text-subtitle-1">Show in home</v-list-item>
+              </v-col>
+              <v-col cols="2" class="d-flex justify-end align-center"> <!-- Ensure vertical alignment -->
+                <v-switch inset color="green" class="small-switch align-center"></v-switch>
+              </v-col>
+            </v-row>
+        <v-select
+              :items="deviceTypes"
+              label="Type"
+              class="pa-3"
+            ></v-select>
+        <v-divider/>
+        <v-card-actions>
+              <v-btn prepend-icon="mdi-delete" variant="tonal" small color="error" dark @click="dialog = false">Delete</v-btn>
+              <v-spacer></v-spacer> 
+              <v-btn variant="tonal" color="secondary" dark @click="cancel">Cancel</v-btn>
+              <v-btn variant="tonal" color="primary" dark @click="dialog = false">Save</v-btn>
+          </v-card-actions>
+      </v-card>
+    </v-col>
+    </v-row>
+    </v-dialog>
 </template>
 <style>
 .background{
@@ -25,3 +56,23 @@ import SprinklerDeviceCard from '@/components/SprinklerDeviceCard.vue';
 }
 </style>
 
+<script>
+import SpeakerDeviceCard from '@/components/SpeakerDeviceCard.vue';
+import AirConditionerDeviceCard from '@/components/SpeakerDeviceCard.vue';
+import SprinklerDeviceCard from '@/components/SprinklerDeviceCard.vue';
+export default {
+  data() {
+    return {
+      dialog: false,
+      deviceTypes: [
+        'Light Panel', 'Control Panel', 'Air Conditioner', 'Speaker', 'Vacuum', 'Oven', 'Fridge'
+      ]
+    };
+  },
+  methods: {
+    cancel() {
+      this.dialog = false;
+    }
+  }
+};
+</script>
