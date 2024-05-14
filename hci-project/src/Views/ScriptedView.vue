eaea3fd4eef368fc36990b2dec7ece1eb0d8618b
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-05-14 20:23:49 -0300

    nueva view

--- hci-project/src/Views/ScriptedView.vue
+++ hci-project/src/Views/ScriptedView.vue
@@ -42,15 +42,17 @@ export default {
 Clave de Dispositivo: :key="device.id" se usa para cada dispositivo dentro de la columna, permitiendo que Vue rastree individualmente cada dispositivo, lo cual es esencial si los dispositivos son agregados, eliminados o reordenados dentro de la columna. -->
 <!-- Eficiencia de Renderizado: Al utilizar claves únicas tanto para columnas como para dispositivos, Vue puede manejar de manera más eficiente las actualizaciones a la interfaz sin necesidad de re-renderizar todo el contenido innecesariamente.
 Escalabilidad: Este enfoque escala bien a medida que agregas más tipos de dispositivos o cambias la estructura de los datos, manteniendo la capacidad de Vue de gestionar estos cambios sin pérdida de rendimiento. -->
-<!-- 
-<template>
+
+<template> hola </template>
+
+<!-- <template>
   <v-app-bar title="Home -- SCRIPTED" color="#DDEAF4" />
   <v-layout class="rounded rounded-md">
     <v-main color="#DDEAF4">
       <v-row class="scrollable" cols="3">
-        <!-- Itera sobre cada columna en devices -->
+        Itera sobre cada columna en devices
         <v-col v-for="(column, index) in device_grid" :key="'col-' + index">
-          <!-- Itera sobre cada dispositivo en la columna -->
+          Itera sobre cada dispositivo en la columna
           <component v-for="device in column" :is="getComponent(device.type)" :key="device.id" class="ma-2"></component>
         </v-col>
       </v-row>
