54e359816761d1adf8020fa7afe03b82028a69dc
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-05-13 22:36:54 -0300

    Fix navbar to preserve pinia store between views:\n change href usage to <router-link>. This avoids reloading the entire site, thus creating a new instance of the store and losing all saved data

--- hci-project/src/components/NavigationBar.vue
+++ hci-project/src/components/NavigationBar.vue
@@ -1,4 +1,8 @@
-<template>
+<!-- 
+  VERSION ANTERIOR DE LA NAVBAR: El uso de href:"item.to" hace que se recargue la 
+  pagina cada vez que se cambia de view, re-instanciando el store y perdiendo los datos!!
+-->
+<!-- <template>
   <v-navigation-drawer color="#D8D7D7" permanent width="194">
     <v-list>
       <v-list-item class="my-2">
@@ -15,8 +19,29 @@
       </v-list-item-group>
     </v-list>      
   </v-navigation-drawer>
-</template>
+</template> -->
 
+<template>
+  <v-navigation-drawer color="#D8D7D7" permanent width="194">
+    <v-list>
+      <v-list-item class="my-2">
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">CoolHome</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item v-for="item in items" :key="item.title" class="drawer-item">
+        <router-link :to="item.to" class="d-flex align-center" style="text-decoration: none; color: inherit; width: 100%;"> 
+            <v-list-item-icon class="mr-4" style="opacity: 0.75;">
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+        </router-link>
+      </v-list-item>
+    </v-list>      
+  </v-navigation-drawer>
+</template>
 
 <script>
   import { ref } from 'vue';
