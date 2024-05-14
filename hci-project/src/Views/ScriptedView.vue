14779b413f768a20d1594c15f7b1bc770dd0f11e
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-05-14 20:07:19 -0300

    restaure scripted view para ver q onda

--- hci-project/src/Views/ScriptedView.vue
+++ hci-project/src/Views/ScriptedView.vue
@@ -1,55 +1,60 @@
-<script>
+<!-- <script>
+import SprinklerDeviceCard from '@/components/SprinklerDeviceCard.vue';
 import Sprinkler from '@/components_composition_api/Sprinkler.vue';
-// import Speaker from '@/components/Speaker.vue';
-import AirConditioner from '@/components/AirConditioner.vue';
+import SpeakerDeviceCard from '@/components/SpeakerDeviceCard.vue';
+import AirConditionerDeviceCard from '@/components/AirConditionerDeviceCard.vue';
 
 export default {
   components: {
+    SprinklerDeviceCard,
     Sprinkler,
-    // Speaker,
-    AirConditioner
+    SpeakerDeviceCard,
+    AirConditionerDeviceCard
+  },
+  data() {
+    return {
+      device_grid: [
+        [{type:'Sprinkler', id: 0}],
+        [{type:'Speaker', id: 1}],
+        [{type:'Sprinkler', id: 2}, {type:'Sprinkler', id: 3}]
+      ]
+    };
+  },
+  methods: {
+  getComponent(type) {
+    const componentMap = {
+      Speaker: SpeakerDeviceCard,
+      AirConditioner: AirConditionerDeviceCard,
+      SprinklerCard: SprinklerDeviceCard,
+      Sprinkler: Sprinkler
+    };
+
+    return componentMap[type] || 'div'; // Un componente por defecto o un mensaje de error
+  },
+  addDevice(device, columnIndex) {
+    this.device_grid[columnIndex].push(device);
   }
-//   data() {
-//     return {
-//       device_grid: [
-//         [{type:'AirConditioner', id: 0}],
-//         [{type:'AirConditioner', id: 1}],
-//         [{type:'AirConditioner', id: 3}],
-//       ]
-//     };
-//   },
-//   methods: {
-//   getComponent(type) {
-//     const componentMap = {
-//       Speaker: SpeakerDeviceCard,
-//       AirConditioner: AirConditionerDeviceCard,
-//       SprinklerCard: SprinklerDeviceCard,
-//       Sprinkler: Sprinkler
-//     };
-//     return componentMap[type] || 'div'; // Un componente por defecto o un mensaje de error
-//   },
-//   addDevice(device, columnIndex) {
-//     this.device_grid[columnIndex].push(device);
-//   }
-// }
+}
 };
-</script>
+</script> -->
 
 <!-- Clave de Columna: :key="'col-' + index" proporciona una clave única para cada columna basada en su posición en el arreglo. Esto es crucial para ayudar a Vue a identificar y gestionar eficientemente cada columna durante las actualizaciones.
 Clave de Dispositivo: :key="device.id" se usa para cada dispositivo dentro de la columna, permitiendo que Vue rastree individualmente cada dispositivo, lo cual es esencial si los dispositivos son agregados, eliminados o reordenados dentro de la columna. -->
 <!-- Eficiencia de Renderizado: Al utilizar claves únicas tanto para columnas como para dispositivos, Vue puede manejar de manera más eficiente las actualizaciones a la interfaz sin necesidad de re-renderizar todo el contenido innecesariamente.
 Escalabilidad: Este enfoque escala bien a medida que agregas más tipos de dispositivos o cambias la estructura de los datos, manteniendo la capacidad de Vue de gestionar estos cambios sin pérdida de rendimiento. -->
-
+<!-- 
 <template>
   <v-app-bar title="Home -- SCRIPTED" color="#DDEAF4" />
   <v-layout class="rounded rounded-md">
     <v-main color="#DDEAF4">
       <v-row class="scrollable" cols="3">
-        <!-- <AirConditionerDeviceCard/> -->
         <!-- Itera sobre cada columna en devices -->
-        
+        <v-col v-for="(column, index) in device_grid" :key="'col-' + index">
+          <!-- Itera sobre cada dispositivo en la columna -->
+          <component v-for="device in column" :is="getComponent(device.type)" :key="device.id" class="ma-2"></component>
+        </v-col>
       </v-row>
       <button @click="addDevice({ type: 'Speaker'}, 0)">Add Speaker to First Column</button>
     </v-main>
   </v-layout>
-</template>
+</template> -->
