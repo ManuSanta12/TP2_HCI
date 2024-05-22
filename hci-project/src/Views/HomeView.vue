f3ab63df85bc82788f6aa5de4b3d9b87c350c401
Manu <msantamarinabalbin@itba.edu.ar>
2024-05-22 10:07:43 -0300

    automation shiw in home

--- hci-project/src/Views/HomeView.vue
+++ hci-project/src/Views/HomeView.vue
@@ -3,9 +3,18 @@
   <v-layout class="rounded rounded-md">
     <v-main color='#DDEAF4'>
       <v-row class="pa-6 scrollable" >
-        <div v-for="device in devices" :key="device.id">
-          <div v-if="device.showInHome">
-            <component :is="getComponent(device.type)" :device="device" />
+        <div v-for="item in filteredItems" :key="item.id">
+          <div >
+                      <component
+                :is="getComponent(item.type)"
+                v-if="isDevice(item)"
+                :device="item"
+              />
+              <AutomationsCard
+                v-else
+                :automation="item"
+                @edit="handleEdit"
+              />
           </div>
         </div> 
       </v-row>
@@ -15,15 +24,19 @@
 
 
 <script setup>
+
 import { computed } from 'vue';
 import AirConditioner from '@/components/AirConditioner.vue';
 import Sprinkler from '@/components/Sprinkler.vue';
 import Speaker from '@/components/Speaker.vue';
 import Light from '@/components/Light.vue';
 import { useDeviceStore } from '@/Stores/DeviceStore';
+import { useAutomationStore } from '@/Stores/AutomationStore';
 
-const store = useDeviceStore();
-const devices = computed(() => store.devices);
+const deviceStore = useDeviceStore();
+const automationStore = useAutomationStore();
+const devices = computed(() => deviceStore.devices);
+const automations = computed(() => automationStore.automations);
 
 function getComponent(type) {
   switch (type) {
@@ -34,4 +47,13 @@ function getComponent(type) {
     default: return 'div'; // Default or unknown type handler
   }
 }
+const isDevice = (item) => {
+  return item && (item.type === 'Air Conditioner' || item.type === 'Light Panel' || item.type === 'Speaker' || item.type === 'Sprinkler');
+}
+
+const filteredItems = computed(() => {
+  const filteredDevices = devices.value.filter(device => device.showInHome);
+  const filteredAutomations = automations.value.filter(automation => automation.showInHome);
+  return [...filteredDevices, ...filteredAutomations];
+});
 </script>
